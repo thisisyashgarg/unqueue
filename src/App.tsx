a81499eb2fe5370d7976f7774972989bef9fbe2d
Yash Garg <thisisyashgarg@gmail.com>
2023-01-30 19:53:48 +0530

    domain added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Domains from "./components/Domains";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Domains />
       {/* yaha jo zaroori hai wo ayega */}
     </>
   );
--- src/components/Domains.tsx
+++ src/components/Domains.tsx
@@ -1,13 +1,23 @@
 import React from "react";
+import { DOMAIN_DATA } from "../data";
 
 export default function Domains() {
   return (
-    <main>
-      {/* yaha 7 domains map karke daal denge */}
-      <div>
-        <img />
-        <h2></h2>
-      </div>
-    </main>
+    <>
+      <h2 className="text-2xl font-medium text-center py-6">
+        Choose you Domain
+      </h2>
+      <main className="flex flex-wrap mx-10 ">
+        {/* yaha 7 domains map karke daal denge */}
+        {DOMAIN_DATA.map((domain) => {
+          return (
+            <div className="flex flex-col py-4">
+              <img src={domain.image} className="w-72 h-48 py-2" />
+              <h2 className="font-medium">{domain.name}</h2>
+            </div>
+          );
+        })}
+      </main>
+    </>
   );
 }
