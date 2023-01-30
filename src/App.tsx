7c013cc128f2716fd14608d8e7d92541b7f28e84
Ankit Pandey <ankitechzz@gmail.com>
2023-01-31 01:19:19 +0530

    almost done with frontend

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Domains from "./components/Domains";
 import Navbar from "./components/Navbar";
+import AdminRegistration from "./components/AdminRegistration";
+import Dashboard from "./components/Dashboard";
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Domains />
+       {/* <Domains /> */}
       {/* yaha jo zaroori hai wo ayega */}
+               {/* <AdminRegistration />  */}
+              {/* <Dashboard /> */}
+              <HomePage />
     </>
   );
 }
