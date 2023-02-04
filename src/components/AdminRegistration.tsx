e0023151902d845dc9c6870e016d790e9291e91a
Yash Garg <thisisyashgarg@gmail.com>
2023-02-04 14:24:31 +0530

    refactored code

--- src/components/AdminRegistration.tsx
+++ src/components/AdminRegistration.tsx
@@ -3,29 +3,19 @@ import { useNavigate } from "react-router-dom";
 import ButtonCTA from "./ButtonCTA";
 import Heading from "./Heading";
 import InputField from "./InputField";
-import axios from "axios";
 import { handleChange } from "../utils/helper";
-
+import { postAdminFormData } from "../utils/postForms";
 
 export default function AdminRegistration() {
   const [adminForm, setAdminForm] = useState({
     orgName: "",
     email: "",
     password: "",
-    ConfirmPassword : ""
+    ConfirmPassword: "",
   });
+  const [showPassword, setShowPassword] = useState(false);
 
-  const [showPassword, setShowPassword] = useState(false);  
   const navigate = useNavigate();
-
-  async function postAdminFormData(e) {
-    e.preventDefault();
-    try {
-      await axios.post("http://localhost:3000/submit/admin", adminForm);
-    } catch (error) {
-      console.error(error);
-    }
-  }
 
   return (
     <div className="flex flex-col items-center p-10">
@@ -34,7 +24,7 @@ export default function AdminRegistration() {
         className="flex flex-col space-y-4 py-10"
         onSubmit={(e) => {
           if (adminForm.password === adminForm.ConfirmPassword) {
-            postAdminFormData(e);
+            postAdminFormData(e, adminForm);
             navigate("/emailsent");
           } else {
             // Show an error message or alert to the user
@@ -48,45 +38,44 @@ export default function AdminRegistration() {
           value={adminForm.orgName}
           onChange={() => handleChange(setAdminForm)}
           name="orgName"
-          type={'text'}
+          type={"text"}
         />
-        {/* <InputField label="Unique ID *" input="ID" /> */}
+
         <InputField
           label="Email ID (for verification) *"
           input="xyz@abc.com"
           value={adminForm.email}
           onChange={() => handleChange(setAdminForm)}
           name="email"
-          type={'text'}
+          type={"text"}
         />
-          <div className="flex">
-  <InputField
-    label="Password *"
-    input="********"
-    value={adminForm.password}
-    onChange={() => handleChange(setAdminForm)}
-    name="password"
-    type={showPassword ? "text" : "password"}
-  />
-  <button
-    type="button"
-    onClick={() => setShowPassword(!showPassword)}
-    className="text-sm"
-  >
-    {showPassword ?"hide"  :"show"}
-  </button>
-</div>
+        {/* <div className="flex "> */}
+        <InputField
+          label="Password *"
+          input="********"
+          value={adminForm.password}
+          onChange={() => handleChange(setAdminForm)}
+          name="password"
+          type={showPassword ? "text" : "password"}
+        />
+        {/* <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+            className="text-sm"
+          >
+            {showPassword ? "Hide" : "Show"}
+          </button> */}
+        {/* </div> */}
 
-          <InputField
-            label="Password *"
-            input="********"
-            value={adminForm.ConfirmPassword}
-            onChange={() => handleChange(setAdminForm)}
-            name="ConfirmPassword"
-            type="password"
-          />
+        <InputField
+          label="Confirm Password *"
+          input="********"
+          value={adminForm.ConfirmPassword}
+          onChange={() => handleChange(setAdminForm)}
+          name="ConfirmPassword"
+          type="password"
+        />
         <ButtonCTA text="Register " />
-
       </form>
     </div>
   );
--- src/components/AdminVerificationPage.tsx
+++ src/components/AdminVerificationPage.tsx
@@ -1,31 +1,24 @@
 import React from "react";
 import Heading from "./Heading";
-import email from '../images/email.svg'
+import email from "../images/email.svg";
 
-
-export default function AdminVerification(){
-  return(
+export default function AdminVerification() {
+  return (
     <div className="grid place-items-center py-12 bg-sky-500">
+      <div className="grid grid-col place-items-center text-center rounded-2xl shadow-xl w-8/12 p-10 pt-20 bg-white">
+        <Heading heading="Verify your email" />
+        <p className="text-xl">
+          Your need to verify your email to complete registration
+        </p>
 
-  <div className="grid grid-col place-items-center text-center rounded-2xl shadow-xl w-8/12 p-14 pt-20 bg-white">
+        <img className="items-center" src={email} alt="" />
 
-     <div className="text-center">
-         <Heading heading="Verify your email" /> 
-         <p className="text-xl">Your need to verify your email to complete registration</p>
-     </div>
-
-         <img className="items-center" src={email} alt="" />
-
-         <div className="bottom-text max-w-2xl">
-          <p className="text-center leading-6">
-            An email has been sent to your email with a link to verify your account .
-             If you have have not recieved the email after a few minutes,
-              please check your spam folder.
-          </p>
-         </div>
-
-  </div>
-  
-  </div>
-  )
+        <p className="text-center leading-6">
+          An email has been sent to your email with a link to verify your
+          account. If you have have not recieved the email after a few minutes,
+          please check your spam folder.
+        </p>
+      </div>
+    </div>
+  );
 }
