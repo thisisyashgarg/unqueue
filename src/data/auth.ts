8c1f677045b98baa8c4801933b9ec75d12011c98
Yash Garg <thisisyashgarg@gmail.com>
2023-02-05 16:12:59 +0530

    apikey protected

--- src/data/auth.ts
+++ src/data/auth.ts
@@ -10,7 +10,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCbf2VhleTZS_ma0c2gK_jl0ojoCF_CCaA",
+  apiKey: `${process.env.API_KEY}`,
   authDomain: "unqueue-548b5.firebaseapp.com",
   databaseURL: "https://unqueue-548b5-default-rtdb.firebaseio.com",
   projectId: "unqueue-548b5",
