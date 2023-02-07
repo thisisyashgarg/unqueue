f63c956d3171d5823476845ec25f65b8cbcaa9af
Yash Garg <thisisyashgarg@gmail.com>
2023-02-07 13:58:13 +0530

    http to https

--- src/data/auth.ts
+++ src/data/auth.ts
@@ -27,7 +27,7 @@ export const auth = getAuth(fireApp);
 export const actionCodeSettings = {
   // URL you want to redirect back to. The domain (www.example.com) for this
   // URL must be in the authorized domains list in the Firebase Console.
-  url: "http://unqueue.netlify.app/dashboard",
+  url: "https://unqueue.netlify.app/dashboard",
   // This must be true.
   handleCodeInApp: true,
   iOS: {
